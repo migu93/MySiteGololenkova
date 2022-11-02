597356d7420e49e1cc074062fed7af99893c28fe
Никита <njkita007@yandex.ru>
2022-11-02 15:11:02 +0300

    до добавления react

---
+++ js/form-validation.js
@@ -0,0 +1,4 @@
+let form = document.querySelector('.js-form'),
+    formInputs = document.querySelectorAll('.js-input'),
+    inputEmail = document.querySelector('.js-input-email'),
+    inputEmail = document.querySelector('.js-input-email');
---
+++ js/regExp.js
@@ -0,0 +1,5 @@
+var regExpMask = IMask(
+    document.getElementById('regexp-mask'),
+    {
+        mask: /^[1-6]\d{0,5}$/
+    });
